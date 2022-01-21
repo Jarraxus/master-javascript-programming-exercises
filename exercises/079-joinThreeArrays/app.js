72b83cecad941e181c98d6383bda514d28c49e4b
Jarraxus <buckman.joshua@gmail.com>
2022-01-21 20:48:07 +0000

    progressed through lesson 79

--- exercises/079-joinThreeArrays/app.js
+++ exercises/079-joinThreeArrays/app.js
@@ -1,3 +1,7 @@
+var output = joinThreeArrays([1, 2], [3, 4], [5, 6]);
+
 function joinThreeArrays(arr1, arr2, arr3) {
-  // your code here
+  return newarr = (arr1.concat(arr2, arr3));
 }
+
+console.log(output); // --> [1, 2, 3, 4, 5, 6]
